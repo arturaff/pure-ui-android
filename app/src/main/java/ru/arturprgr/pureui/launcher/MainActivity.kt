1e6bbeec28f146e3ac69226cb8c3d9c25af64327
arturaff <arturars2022@gmail.com>
2024-09-21 11:31:15 +0500

    Добавлен диалог с просьбой назначить приложение лаунчером

--- app/src/main/java/ru/arturprgr/pureui/launcher/MainActivity.kt
+++ app/src/main/java/ru/arturprgr/pureui/launcher/MainActivity.kt
@@ -1,8 +1,13 @@
 package ru.arturprgr.pureui.launcher
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -16,6 +21,7 @@ import ru.arturprgr.pureui.launcher.screens.MainFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sharedPreferences: SharedPreferences
     private lateinit var viewPagerAdapter: ViewPagerAdapter
 
     @SuppressLint("SetTextI18n", "ResourceAsColor")
@@ -23,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
+        sharedPreferences = getSharedPreferences("sPrefs", Context.MODE_PRIVATE)
         viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
         Singleton.viewPagerAdapter = viewPagerAdapter
         setContentView(binding.root)
@@ -30,6 +37,19 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        if (!sharedPreferences.getBoolean("is_default", false)) {
+            AlertDialog.Builder(this@MainActivity)
+                .setTitle(resources.getString(R.string.select_your_default_home_screen))
+                .setMessage(resources.getString(R.string.to_use_the_launcher_you_need_to_select_the_PureUI_application_as_your_default_home_screen))
+                .setPositiveButton(resources.getString(R.string.сhoose)) { _, _ ->
+                    sharedPreferences.edit().putBoolean("is_default", true).apply()
+                    startActivity(Intent(Settings.ACTION_HOME_SETTINGS))
+                }
+                .setNegativeButton(resources.getString(R.string.no)) { _, _ -> }
+                .create()
+                .show()
         }
 
         binding.apply {
